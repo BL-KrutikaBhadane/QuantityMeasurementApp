2f969f0dcf60869166759883cb030ba03db92624
BL-KrutikaBhadane <krutika.bhadane@bridgelabz.com>
2026-04-13 14:11:50 +0530

    Added. UC8 Conversion responsibility

--- src/main/java/org/example/QuantityMeasurementApp.java
+++ src/main/java/org/example/QuantityMeasurementApp.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.example.Length.LengthUnit;
+import org.example.LengthUnit;
 public class QuantityMeasurementApp {
 
     public static boolean demonstrateLengthEquality(Length length1, Length length2){
@@ -33,6 +33,10 @@ public class QuantityMeasurementApp {
         return length1.add(length2);
     }
 
+    public static Length demonstrateLengthAddition(Length length1, Length length2, LengthUnit targetLength){
+        return length1.add(length2, targetLength);
+    }
+
     public static void main(String[] args) {
         Length length1 = new Length(1.0, LengthUnit.FEET);
         Length length2 = new Length(12.0, LengthUnit.INCHES);
@@ -44,5 +48,6 @@ public class QuantityMeasurementApp {
         System.out.println(demonstrateLengthConversion(5.0, LengthUnit.YARDS, LengthUnit.INCHES));
         System.out.println(demonstrateLengthConversion(length1, LengthUnit.INCHES));
         System.out.println(length1+" + "+length2+" = "+demonstrateLengthAddition(length1, length2));
+        System.out.println(length1+" + "+length2+" = "+demonstrateLengthAddition(length1, length2, LengthUnit.CENTIMETERS));
     }
 }
